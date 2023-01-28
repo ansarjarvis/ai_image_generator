36ab5608525ad5edff831f5ea0d8a0e6a47f1238
Mohammad Ansar <ansar.ofcl00@gmail.com>
2023-01-28 13:11:57 +0530

    add the home page/scene

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,20 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Home from "./scenes/home/index";
+import Post from "./scenes/post/index";
+import Header from "./components/Header";
 const App = () => {
-  return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-post" element={<Post />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </>
+  );
 };
 export default App;
---
+++ client/src/components/RenderCard.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card";
+
+let RenderCard = ({ data, title }) => {
+  if (data.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  } else {
+    return (
+      <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+        {title}
+      </h2>
+    );
+  }
+};
+export default RenderCard;
